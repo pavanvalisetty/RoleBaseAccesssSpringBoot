23fd2d1d93e6291e8c91f5db2b3f99f2d6d8c0f8
pavanvalisetty <pavanvalisetty@gmail.com>
2024-08-21 18:53:00 +0530

    first commit

---
+++ src/main/java/com/alpha/controller/EmployeeController.java
@@ -0,0 +1,52 @@
+package com.alpha.controller;
+
+import com.alpha.config.TokenProvider;
+import com.alpha.model.AuthToken;
+import com.alpha.model.LoginUser;
+import com.alpha.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+
+@CrossOrigin(origins = "**", maxAge = 3600)
+@RestController
+@RequestMapping("/emp")
+public class EmployeeController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private TokenProvider jwtTokenUtil;
+
+    @Autowired
+    private UserService userService;
+
+
+    /**
+     * Generates a token for the given user credentials.
+     *
+     * @param loginUser The user's login credentials.
+     * @return A response entity containing the generated token.
+     * @throws AuthenticationException if authentication fails.
+     */
+    @RequestMapping(value = "/authenticate", method = RequestMethod.POST)
+    public ResponseEntity<?> generateToken(@RequestBody LoginUser loginUser) throws AuthenticationException {
+        final Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginUser.getUsername(),
+                        loginUser.getPassword()
+                )
+        );
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        final String token = jwtTokenUtil.generateToken(authentication);
+        return ResponseEntity.ok(new AuthToken(token));
+    }
+}
+
